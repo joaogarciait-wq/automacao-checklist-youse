5d02a348ce0f96077f491f2cc52a8e65bc38db94
joaogarciait-wq <joaogarciait@gmail.com>
2025-08-27 09:11:29 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Adiciona o diretório src ao PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:/opt/render/project/src
-
-# Inicia o Gunicorn
-python -m gunicorn --bind 0.0.0.0:$PORT src.main:app
-
-
